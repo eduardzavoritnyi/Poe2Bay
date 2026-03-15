39126b9a554775cc5dfd5af6043374ef0ab6110e
jscripter21 <jscripter21@gmail.com>
2026-03-15 16:15:37 +0200

    feat: add custom hooks and WooCommerce structure

--- wp-content/themes/poe2bay/functions.php
+++ wp-content/themes/poe2bay/functions.php
@@ -1,5 +1,10 @@
  <?php 
 
  add_action('after_setup_theme', function(){
-    add_theme_support('woocommerce');
+   add_theme_support('woocommerce');
+
+   remove_theme_support( 'wc-product-gallery-zoom' );
+   remove_theme_support( 'wc-product-gallery-lightbox' );
+   remove_theme_support( 'wc-product-gallery-slider' );  
+   remove_theme_support('title-tag');
  });
